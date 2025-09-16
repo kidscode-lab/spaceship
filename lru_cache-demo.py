91af159f108f27ebbb75a539a6dd58f552f9e3f3
quincykong <quincykong@gmail.com>
2025-09-16 16:43:13 -0400

    Refresh .gitignore

--- lru_cache-demo.py
+++
@@ -1,9 +0,0 @@
-from functools import lru_cache
-
-@lru_cache(maxsize=None)
-def slow_square(n):
-    print(f"Calculating {n}²...")
-    return n * n
-
-print(slow_square(5))  # calculates
-print(slow_square(5))  # instant, cached
